16fc03b619a58675d732504883a5c2a8c8487007
Mendel Gordon <mendelgordons@gmail.com>
2023-11-29 15:04:12 -0500

    Fix scroll direction in makeScrollable function

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -3,31 +3,31 @@ const co_photo_gallery = document.querySelector('.co_photo_gallery');
 const slider_wrapper = document.querySelector('.slider-wrapper');
 
 const makeScrollable = (element) => {
-    if (element) {
-        let position = {
-            left: 0,
-            x: 0
-        }
-        const mouseDownHandler = (e) => {
-            element.style.cursor = 'grabbing';
-            element.style.userSelect = 'none';
-            position.left = element.scrollLeft;
-            position.x = e.clientX;
-            document.addEventListener('mousemove', mouseMoveHandler);
-            document.addEventListener('mouseup', mouseUpHandler);
-        }
-        const mouseMoveHandler = (e) => {
-            element.scrollLeft = position.left + e.clientX - position.x;
-        }
-        const mouseUpHandler = () => {
-            element.style.cursor = 'grab';
-            element.style.userSelect = '';
-            document.removeEventListener('mousemove', mouseMoveHandler);
-            document.removeEventListener('mouseup', mouseUpHandler);
-        }
-        element.addEventListener('mousedown', mouseDownHandler);
-    }
-}
+	if (element) {
+		let position = {
+			left: 0,
+			x: 0
+		};
+		const mouseDownHandler = (e) => {
+			element.style.cursor = 'grabbing';
+			element.style.userSelect = 'none';
+			position.left = element.scrollLeft;
+			position.x = e.clientX;
+			document.addEventListener('mousemove', mouseMoveHandler);
+			document.addEventListener('mouseup', mouseUpHandler);
+		};
+		const mouseMoveHandler = (e) => {
+			element.scrollLeft = position.left - e.clientX + position.x;
+		};
+		const mouseUpHandler = () => {
+			element.style.cursor = 'grab';
+			element.style.userSelect = '';
+			document.removeEventListener('mousemove', mouseMoveHandler);
+			document.removeEventListener('mouseup', mouseUpHandler);
+		};
+		element.addEventListener('mousedown', mouseDownHandler);
+	}
+};
 
 makeScrollable(co_photo_gallery);
 makeScrollable(slider_wrapper);
