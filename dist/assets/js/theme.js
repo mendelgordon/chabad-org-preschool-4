ebd4b216858f3533189780b72ad5980a3f1e6d4e
Mendel Gordon <mendelgordons@gmail.com>
2023-11-29 14:57:13 -0500

    Refactor scrollable functionality for sections

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -1,26 +1,33 @@
-// make .co_photo_gallery scrollable by dragging on the page on desktop
+// make sections scrollable by dragging on the page even on desktop
 const co_photo_gallery = document.querySelector('.co_photo_gallery');
-if (co_photo_gallery) {
-    let position = {
-        left: 0,
-        x: 0
+const slider_wrapper = document.querySelector('.slider-wrapper');
+
+const makeScrollable = (element) => {
+    if (element) {
+        let position = {
+            left: 0,
+            x: 0
+        }
+        const mouseDownHandler = (e) => {
+            element.style.cursor = 'grabbing';
+            element.style.userSelect = 'none';
+            position.left = element.scrollLeft;
+            position.x = e.clientX;
+            document.addEventListener('mousemove', mouseMoveHandler);
+            document.addEventListener('mouseup', mouseUpHandler);
+        }
+        const mouseMoveHandler = (e) => {
+            element.scrollLeft = position.left + e.clientX - position.x;
+        }
+        const mouseUpHandler = () => {
+            element.style.cursor = 'grab';
+            element.style.userSelect = '';
+            document.removeEventListener('mousemove', mouseMoveHandler);
+            document.removeEventListener('mouseup', mouseUpHandler);
+        }
+        element.addEventListener('mousedown', mouseDownHandler);
     }
-    const mouseDownHandler = (e) => {
-        co_photo_gallery.style.cursor = 'grabbing';
-        co_photo_gallery.style.userSelect = 'none';
-        position.left = co_photo_gallery.scrollLeft;
-        position.x = e.clientX;
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-    }
-    const mouseMoveHandler = (e) => {
-        co_photo_gallery.scrollLeft = position.left + e.clientX - position.x;
-    }
-    const mouseUpHandler = () => {
-        co_photo_gallery.style.cursor = 'grab';
-        co_photo_gallery.style.userSelect = '';
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler);
-    }
-    co_photo_gallery.addEventListener('mousedown', mouseDownHandler);
 }
+
+makeScrollable(co_photo_gallery);
+makeScrollable(slider_wrapper);
