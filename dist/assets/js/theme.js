6f1e89671e3ebdd80f542d75e46cea4007a9b318
Mendel Gordon <mendelgordons@gmail.com>
2023-12-15 00:13:26 -0500

    Update scroll behavior in theme.js to add snapping back

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -2,7 +2,7 @@
 const co_photo_gallery = document.querySelector('.co_photo_gallery');
 const slider_wrapper = document.querySelector('.slider-wrapper');
 
-const makeScrollable = (element) => {
+const makeScrollable = (element, childSelector) => {
 	if (element) {
 		let position = {
 			left: 0,
@@ -14,6 +14,11 @@ const makeScrollable = (element) => {
 			element.style.userSelect = 'none';
 			position.left = element.scrollLeft;
 			position.x = e.clientX;
+			element.style.scrollSnapType = '';
+			const children = element.querySelectorAll(childSelector);
+			children.forEach(child => {
+				child.style.scrollSnapAlign = '';
+			});
 			document.addEventListener('mousemove', mouseMoveHandler);
 			document.addEventListener('mouseup', mouseUpHandler);
 		};
@@ -23,6 +28,11 @@ const makeScrollable = (element) => {
 		const mouseUpHandler = () => {
 			element.style.cursor = 'grab';
 			element.style.userSelect = '';
+			element.style.scrollSnapType = 'x mandatory';
+			const children = element.querySelectorAll(childSelector);
+			children.forEach(child => {
+				child.style.scrollSnapAlign = 'start';
+			});
 			document.removeEventListener('mousemove', mouseMoveHandler);
 			document.removeEventListener('mouseup', mouseUpHandler);
 		};
@@ -30,8 +40,8 @@ const makeScrollable = (element) => {
 	}
 };
 
-makeScrollable(co_photo_gallery);
-makeScrollable(slider_wrapper);
+makeScrollable(co_photo_gallery, 'img');
+makeScrollable(slider_wrapper, '.slide');
 
 const photos = document.querySelectorAll('.co_photo_gallery a');
 photos.forEach((photo) => {
