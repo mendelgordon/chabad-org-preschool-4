21f9a34ce5edd6dd88a5072cb38abf52589e7b76
Mendel Gordon <mendelgordons@gmail.com>
2023-12-15 11:33:51 -0500

    Add wrappers

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -1,6 +1,9 @@
-// make sections scrollable by dragging on the page even on desktop
-const co_photo_gallery = document.querySelectorAll('.co_photo_gallery');
-co_photo_gallery?.classList.add('swiper');
+const co_photo_gallery = document.querySelector('.co_photo_gallery');
+if (co_photo_gallery) {
+	co_photo_gallery.classList.add('swiper-wrapper');
+	// wrap co_photo_gallery in div with swiper class to make it work with swiper js
+	co_photo_gallery.parentElement.classList.add('swiper');
+}
 
 function createSwiper(selector) {
 	return new Swiper(selector, {
@@ -15,9 +18,10 @@ function createSwiper(selector) {
 			800: {
 				slidesPerView: 3
 			}
-		}
+		},
+		navigation: true,
 	});
 }
 
 const swiper = createSwiper('.swiper');
-const swiper2 = createSwiper('.co_photo_gallery');
+const swiper2 = createSwiper('.co_photo_gallery_gradient');
