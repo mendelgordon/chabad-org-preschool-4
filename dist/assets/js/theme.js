cae63ec9d82a32e9ee490926ffc3e97059be18b8
Mendel Gordon <mendelgordons@gmail.com>
2024-01-03 10:42:48 -0500

    Refactor Swiper initialization in theme.js - add nav arrows

--- dist/assets/js/theme.js
+++ dist/assets/js/theme.js
@@ -5,10 +5,11 @@ if (co_photo_gallery) {
 	co_photo_gallery.querySelectorAll('a').forEach(el => el.classList.add('swiper-slide'));
 }
 
-function createSwiper(selector) {
-	if (!document.querySelector(selector)) return console.error(`Swiper selector ${selector} not found`);
-	if (!Swiper) return console.error('Swiper not found');
-	return new Swiper(selector, {
+const swiperEl1 = document.querySelector('#Testimonials .swiper');
+const swiperEl2 = document.querySelector('.co_photo_gallery_gradient');
+
+if (swiperEl1 && Swiper) {
+	var swiper1 = createSwiper(swiperEl1, {
 		grabCursor: true,
 		breakpoints: {
 			0: { slidesPerView: 1.3 },
@@ -23,8 +24,34 @@ function createSwiper(selector) {
 			rotate: 0,
 		},
 		loop: true,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '#Testimonials .swiper-button-next',
+			prevEl: '#Testimonials .swiper-button-prev',
+		}
 	});
 }
 
-const swiper = createSwiper('.swiper');
-const swiper2 = createSwiper('.co_photo_gallery_gradient');
+if (swiperEl2 && Swiper) {
+	var swiper2 = createSwiper(swiperEl2, {
+		grabCursor: true,
+		breakpoints: {
+			0: { slidesPerView: 1.3 },
+			850: { slidesPerView: 2 },
+			1100: { slidesPerView: 3 }
+		},
+		autoHeight: true,
+		effect: 'coverflow',
+		coverflowEffect: {
+			slideShadows: false,
+			scale: 0.9,
+			rotate: 0,
+		},
+		loop: true,
+		centeredSlides: true,
+		navigation: {
+			nextEl: '.co_photo_gallery_gradient .swiper-button-next',
+			prevEl: '.co_photo_gallery_gradient .swiper-button-prev',
+		}
+	});
+}
