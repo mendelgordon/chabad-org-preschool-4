eab4da3a36416e6fdfc29fec67d94dd0a5846523
Mendel Gordon <mendelgordons@gmail.com>
2024-01-10 09:47:17 -0500

    Fix class function

--- public/theme.js
+++ public/theme.js
@@ -1,68 +1,68 @@
 const co_photo_gallery = document.querySelector('.co_photo_gallery');
+function addSwiperClass(el) {
+    return el.classList.add('swiper-slide');
+}
+
 if (co_photo_gallery) {
- const parentElement = co_photo_gallery.parentElement;
- const navigationButtonNext = document.createElement('div');
- navigationButtonNext.classList.add('swiper-button-next');
- const navigationButtonPrev = document.createElement('div');
- navigationButtonPrev.classList.add('swiper-button-prev');
- parentElement.insertAdjacentElement('afterend', navigationButtonPrev);
- parentElement.insertAdjacentElement('afterend', navigationButtonNext);
- parentElement.classList.add('swiper');
- co_photo_gallery.classList.add('swiper-wrapper');
- co_photo_gallery.querySelectorAll('a').forEach(addSwiperClass(el));
+    const parentElement = co_photo_gallery.parentElement;
+    const navigationButtonNext = document.createElement('div');
+    navigationButtonNext.classList.add('swiper-button-next');
+    const navigationButtonPrev = document.createElement('div');
+    navigationButtonPrev.classList.add('swiper-button-prev');
+    parentElement.insertAdjacentElement('afterend', navigationButtonPrev);
+    parentElement.insertAdjacentElement('afterend', navigationButtonNext);
+    parentElement.classList.add('swiper');
+    co_photo_gallery.classList.add('swiper-wrapper');
+    co_photo_gallery.querySelectorAll('a').forEach(addSwiperClass);
 }
 
 const swiperEl1 = document.querySelector('#Testimonials .swiper');
 const swiperEl2 = document.querySelector('.co_photo_gallery_gradient');
 
 if (Swiper) {
- if (swiperEl1) {
-  var swiper1 = new Swiper(swiperEl1, {
-   grabCursor: true,
-   breakpoints: {
-    0: { slidesPerView: 1.3 },
-    850: { slidesPerView: 2 },
-    1100: { slidesPerView: 3 }
-   },
-   autoHeight: true,
-   effect: 'coverflow',
-   coverflowEffect: {
-    slideShadows: false,
-    scale: 0.9,
-    rotate: 0,
-   },
-   loop: true,
-   navigation: {
-    nextEl: '#Testimonials .swiper-button-next',
-    prevEl: '#Testimonials .swiper-button-prev',
-   }
-  });
- }
+    if (swiperEl1) {
+        var swiper1 = new Swiper(swiperEl1, {
+            grabCursor: true,
+            breakpoints: {
+                0: { slidesPerView: 1.3 },
+                850: { slidesPerView: 2 },
+                1100: { slidesPerView: 3 }
+            },
+            autoHeight: true,
+            effect: 'coverflow',
+            coverflowEffect: {
+                slideShadows: false,
+                scale: 0.9,
+                rotate: 0,
+            },
+            loop: true,
+            navigation: {
+                nextEl: '#Testimonials .swiper-button-next',
+                prevEl: '#Testimonials .swiper-button-prev',
+            }
+        });
+    }
 
- if (swiperEl2) {
-  var swiper2 = new Swiper(swiperEl2, {
-   grabCursor: true,
-   breakpoints: {
-    0: { slidesPerView: 1.3 },
-    850: { slidesPerView: 2 },
-    1100: { slidesPerView: 3 }
-   },
-   autoHeight: true,
-   effect: 'coverflow',
-   coverflowEffect: {
-    slideShadows: false,
-    scale: 0.9,
-    rotate: 0,
-   },
-   loop: true,
-   navigation: {
-    nextEl: '#PreschoolLatestPhotos .swiper-button-next',
-    prevEl: '#PreschoolLatestPhotos .swiper-button-prev',
-   }
-  });
- }
-}
-
-function addSwiperClass(el) {
- return el.classList.add('swiper-slide');
+    if (swiperEl2) {
+        var swiper2 = new Swiper(swiperEl2, {
+            grabCursor: true,
+            breakpoints: {
+                0: { slidesPerView: 1.3 },
+                850: { slidesPerView: 2 },
+                1100: { slidesPerView: 3 }
+            },
+            autoHeight: true,
+            effect: 'coverflow',
+            coverflowEffect: {
+                slideShadows: false,
+                scale: 0.9,
+                rotate: 0,
+            },
+            loop: true,
+            navigation: {
+                nextEl: '#PreschoolLatestPhotos .swiper-button-next',
+                prevEl: '#PreschoolLatestPhotos .swiper-button-prev',
+            }
+        });
+    }
 }
